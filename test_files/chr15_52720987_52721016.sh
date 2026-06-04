03ab8f2b20fbe6e69a39d6066645184be0603462
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-06-04 10:15:50 +0300

    Better test scripts.

--- test_files/chr15_52720987_52721016.sh
+++ test_files/chr15_52720987_52721016.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-cargo build
-
-target/debug/tsalign align -p test_files/chr15_52720987_52721016.fa -o test_files/chr15_52720987_52721016.toml --alignment-method a-star-template-switch --skip-characters N- --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price --allow-ts-14-out-of-range -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R490..555Q490..542
+cargo run -- align -p test_files/chr15_52720987_52721016.fa -o test_files/chr15_52720987_52721016.toml --alignment-method a-star-template-switch --skip-characters N- --alphabet dna -c test_files/config/bench --ts-node-ord-strategy anti-diagonal --ts-min-length-strategy preprocess-price --allow-ts-14-out-of-range -k 0 --max-chaining-successors 0 --max-exact-cost-function-cost 0 --chaining-closed-list special --chaining-open-list linear-heap --rq-ranges R490..555Q490..542
