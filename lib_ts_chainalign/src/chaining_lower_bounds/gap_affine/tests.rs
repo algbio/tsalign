b6d03347d7e1a6bbdc87d10801ba29c28650941e
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2026-09-25 10:11:06 +0300

    Test inexact lower bounds.

--- lib_ts_chainalign/src/chaining_lower_bounds/gap_affine/tests.rs
+++ lib_ts_chainalign/src/chaining_lower_bounds/gap_affine/tests.rs
@@ -332,3 +332,185 @@ fn exact_max_match_run_2_allow_end_match() {
         }
     }
 }
+
+#[test]
+fn inexact_1_0() {
+    let cost_table = GapAffineCosts {
+        substitution: U32Cost::from(2u8),
+        gap_open: U32Cost::from(3u8),
+        gap_extend: U32Cost::from(1u8),
+    };
+    let max_n = 2;
+    let lower_bounds = GapAffineLowerBounds::new_inexact_anchors(max_n, 1, 0, &cost_table);
+
+    #[rustfmt::skip]
+    let exepcted_lower_bounds = Array2::from_shape_vec(
+        (max_n+1, max_n+1),
+        vec![
+            0, 3, 4,
+            3, 2, 5,
+            4, 5, 4,
+        ],
+    )
+    .unwrap();
+
+    for a in 0..=max_n {
+        for b in 0..=max_n {
+            assert_eq!(
+                lower_bounds.lower_bound(a, b).as_primitive(),
+                exepcted_lower_bounds[[a, b]],
+                "lower bound({}, {})",
+                a,
+                b
+            );
+        }
+    }
+}
+
+#[test]
+fn inexact_2_0() {
+    let cost_table = GapAffineCosts {
+        substitution: U32Cost::from(2u8),
+        gap_open: U32Cost::from(3u8),
+        gap_extend: U32Cost::from(1u8),
+    };
+    let max_n = 4;
+    let lower_bounds = GapAffineLowerBounds::new_inexact_anchors(max_n, 2, 0, &cost_table);
+
+    #[rustfmt::skip]
+    let exepcted_lower_bounds = Array2::from_shape_vec(
+        (max_n+1, max_n+1),
+        vec![
+            0, 3, 4, 5, 6,
+            3, 0, 3, 4, 5,
+            4, 3, 2, 3, 4,
+            5, 4, 3, 2, 5,
+            6, 5, 4, 5, 4,
+        ],
+    )
+    .unwrap();
+
+    for a in 0..=max_n {
+        for b in 0..=max_n {
+            assert_eq!(
+                lower_bounds.lower_bound(a, b).as_primitive(),
+                exepcted_lower_bounds[[a, b]],
+                "lower bound({}, {})",
+                a,
+                b
+            );
+        }
+    }
+}
+
+#[test]
+fn inexact_3_0() {
+    let cost_table = GapAffineCosts {
+        substitution: U32Cost::from(2u8),
+        gap_open: U32Cost::from(3u8),
+        gap_extend: U32Cost::from(1u8),
+    };
+    let max_n = 6;
+    let lower_bounds = GapAffineLowerBounds::new_inexact_anchors(max_n, 3, 0, &cost_table);
+
+    #[rustfmt::skip]
+    let exepcted_lower_bounds = Array2::from_shape_vec(
+        (max_n+1, max_n+1),
+        vec![
+            0, 3, 4, 5, 6, 7, 8,
+            3, 0, 3, 4, 5, 6, 7,
+            4, 3, 0, 3, 4, 5, 6,
+            5, 4, 3, 2, 3, 4, 5,
+            6, 5, 4, 3, 2, 3, 4,
+            7, 6, 5, 4, 3, 2, 5,
+            8, 7, 6, 5, 4, 5, 4,
+        ],
+    )
+    .unwrap();
+
+    for a in 0..=max_n {
+        for b in 0..=max_n {
+            assert_eq!(
+                lower_bounds.lower_bound(a, b).as_primitive(),
+                exepcted_lower_bounds[[a, b]],
+                "lower bound({}, {})",
+                a,
+                b
+            );
+        }
+    }
+}
+
+#[test]
+fn inexact_2_1() {
+    let cost_table = GapAffineCosts {
+        substitution: U32Cost::from(2u8),
+        gap_open: U32Cost::from(3u8),
+        gap_extend: U32Cost::from(1u8),
+    };
+    let max_n = 4;
+    let lower_bounds = GapAffineLowerBounds::new_inexact_anchors(max_n, 2, 1, &cost_table);
+
+    #[rustfmt::skip]
+    let exepcted_lower_bounds = Array2::from_shape_vec(
+        (max_n+1, max_n+1),
+        vec![
+            0, 3, 4, 5, 6,
+            3, 0, 5, 6, 7,
+            4, 5, 4, 7, 8,
+            5, 6, 7, 6, 9,
+            6, 7, 8, 9, 8,
+        ],
+    )
+    .unwrap();
+
+    for a in 0..=max_n {
+        for b in 0..=max_n {
+            assert_eq!(
+                lower_bounds.lower_bound(a, b).as_primitive(),
+                exepcted_lower_bounds[[a, b]],
+                "lower bound({}, {})",
+                a,
+                b
+            );
+        }
+    }
+}
+
+#[test]
+fn inexact_3_1() {
+    let cost_table = GapAffineCosts {
+        substitution: U32Cost::from(2u8),
+        gap_open: U32Cost::from(3u8),
+        gap_extend: U32Cost::from(1u8),
+    };
+    let max_n = 6;
+    let lower_bounds = GapAffineLowerBounds::new_inexact_anchors(max_n, 3, 1, &cost_table);
+
+    #[rustfmt::skip]
+    let exepcted_lower_bounds = Array2::from_shape_vec(
+        (max_n+1, max_n+1),
+        vec![
+            0, 3, 4, 5, 6, 7, 8,
+            3, 0, 3, 4, 5, 6, 7,
+            4, 3, 0, 5, 4, 5, 6,
+            5, 4, 5, 4, 5, 6, 7,
+            6, 5, 4, 5, 4, 7, 8,
+            7, 6, 5, 6, 7, 6, 9,
+            8, 7, 6, 7, 8, 9, 8,
+        ],
+    )
+    .unwrap();
+
+    for a in 0..=max_n {
+        for b in 0..=max_n {
+            assert_eq!(
+                lower_bounds.lower_bound(a, b).as_primitive(),
+                exepcted_lower_bounds[[a, b]],
+                "lower bound({}, {})",
+                a,
+                b
+            );
+        }
+    }
+}
